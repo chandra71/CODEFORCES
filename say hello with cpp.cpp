978f97ff0e3f721ac3dbf4c1eb1da8386d831d46
chandu_116 <118456799+chandra71@users.noreply.github.com>
2025-04-28 23:18:22 +0530

    Add files via upload

---
+++ say hello with cpp.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // INCLUDE INPUT/OUTPUT LIBRARY
+#include <string>    // INCLUDE STRING LIBRARY
+using namespace std;
+
+int main() {
+    string name; // DECLARE A VARIABLE TO STORE THE INPUT NAME
+    cin >> name; // READ THE INPUT NAME FROM THE USER
+    cout << "Hello, " << name << endl; // PRINT "HEL
