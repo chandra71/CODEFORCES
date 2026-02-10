54bee4a8b8ffde4f36e3c63a669899e2ae420497
chandu_116 <118456799+chandra71@users.noreply.github.com>
2026-02-10 23:56:34 +0530

    Add files via upload

---
+++ I Wanna Be the Guy.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    set<int> levels;   // To store unique levels they can pass
+
+    int p;
+    cin >> p;
+    for(int i = 0; i < p; i++) {
+        int x;
+        cin >> x;
+        levels.insert(x);
+    }
+
+    int q;
+    cin >> q;
+    for(int i = 0; i < q; i++) {
+        int y;
+        cin >> y;
+        levels.insert(y);
+    }
+
+    if(levels.size() == n)
+        cout << "I become the guy." << endl;
+    else
+        cout << "Oh, my keyboard!" << endl;
+
+    return 0;
+}
